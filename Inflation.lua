d4864f1b675434d6674fb7624af63687b55437c6
YeetML <kongispro245@gmail.com>
2026-09-06 00:19:19 +0800

    Create Inflation.lua

---
+++ Inflation.lua
@@ -0,0 +1,228 @@
+TDS:Loadout("DJ Booth", "Gatling Gun", "Medic", "Ace Pilot", "Trapper")
+TDS:Mode("Trial")
+TDS:Ready()
+
+-- [ Wave 1 ] --
+TDS:VoteSkip(1)
+TDS:Place("Ace Pilot", -26.042499542236328, 1.0470643043518066, -0.28392988443374634)
+TDS:Ability(1, "Toggle Reverse")
+
+-- [ Wave 3 ] --
+TDS:Place("Trapper", -26.771717071533203, 1.0000016689300537, -10.249460220336914)
+TDS:Upgrade(2)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 5 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 7 ] --
+TDS:Upgrade(2)
+
+-- [ Wave 8 ] --
+TDS:WaitForWave(8)
+TDS:SetOption(2, "Trap", "Landmine")
+TDS:SetTarget(2, "Last")
+TDS:SetTarget(2, "Strongest")
+TDS:SetTarget(2, "Weakest")
+TDS:SetTarget(2, "Closest")
+
+-- [ Wave 10 ] --
+TDS:VoteSkip(10)
+TDS:Sell(1)
+
+-- [ Wave 11 ] --
+TDS:WaitForWave(11)
+TDS:Sell(2)
+TDS:Place("Gatling Gun", -29.07954978942871, 1.026092529296875, -36.183162689208984)
+TDS:Place("DJ Booth", -21.382394790649414, 1.000004529953003, -28.071731567382812)
+TDS:SetOption(4, "Track", "Green")
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(3)
+TDS:SetOption(4, "Track", "Red")
+
+-- [ Wave 13 ] --
+TDS:Upgrade(4)
+TDS:SetOption(4, "Track", "Green")
+
+-- [ Wave 14 ] --
+TDS:Place("Trapper", -18.311573028564453, 1.0000050067901611, -30.885513305664062)
+TDS:Place("Trapper", -15.203198432922363, 1.030224084854126, -30.747081756591797)
+TDS:Place("Trapper", -12.018299102783203, 1.0260766744613647, -30.99932861328125)
+
+-- [ Wave 15 ] --
+TDS:Place("Trapper", -16.875947952270508, 1.000004529953003, -28.205371856689453)
+TDS:Place("Trapper", -13.432424545288086, 1.000004529953003, -28.073362350463867)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(3)
+TDS:Place("Trapper", -8.91378402709961, 1.026076316833496, -30.850820541381836)
+TDS:Place("Trapper", -10.382735252380371, 1.000004529953003, -28.013290405273438)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(4)
+
+-- [ Wave 18 ] --
+TDS:Place("Medic", -22.578683853149414, 1.0260931253433228, -36.2923698425293)
+TDS:Place("Medic", -19.535865783691406, 1.0325613021850586, -36.04930114746094)
+TDS:Place("Medic", -22.65554428100586, 1.026103138923645, -39.4172248840332)
+TDS:Place("Medic", -19.497020721435547, 1.02610182762146, -39.0958366394043)
+
+-- [ Wave 19 ] --
+TDS:Place("Ace Pilot", -10.756641387939453, 1.0161556005477905, -21.100000381469727)
+TDS:Place("Ace Pilot", -10.875890731811523, 0.6000033020973206, -24.2362003326416)
+TDS:Place("Ace Pilot", -13.556937217712402, 0.6000030040740967, -22.50008773803711)
+TDS:Place("Ace Pilot", -16.557613372802734, 0.6000030636787415, -22.76143455505371)
+TDS:Place("Ace Pilot", -19.74884033203125, 0.6000030040740967, -22.682830810546875)
+TDS:Place("Ace Pilot", -18.75877571105957, 0.6000024080276489, -18.981773376464844)
+
+-- [ Wave 20 ] --
+TDS:Place("Ace Pilot", -19.044082641601562, 0.600001871585846, -15.643265724182129)
+TDS:Place("Ace Pilot", -22.804452896118164, 0.6000031232833862, -23.081941604614258)
+TDS:Upgrade(3)
+
+-- [ Wave 22 ] --
+TDS:Upgrade(4)
+
+-- [ Wave 25 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+
+-- [ Wave 26 ] --
+
+-- [ Wave 27 ] --
+TDS:Upgrade(3)
+
+
+-- [ Wave 28 ] --
+
+
+-- [ Wave 29 ] --
+
+
+-- [ Wave 30 ] --
+
+TDS:Upgrade(3)
+
+-- [ Wave 31 ] --
+
+
+-- [ Wave 32 ] --
+
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+
+-- [ Wave 33 ] --
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:SetOption(5, "Trap", "Landmine")
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:SetOption(6, "Trap", "Bear Traps")
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:SetOption(7, "Trap", "Landmine")
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:SetOption(10, "Trap", "Bear Traps")
+
+-- [ Wave 34 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:SetOption(8, "Trap", "Landmine")
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:SetOption(9, "Trap", "Bear Traps")
+TDS:Upgrade(11)
+
+-- [ Wave 35 ] --
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:SetOption(11, "Trap", "Landmine")
+
+-- [ Wave 36 ] --
+TDS:Upgrade(21)
+TDS:Upgrade(21)
+TDS:Upgrade(21)
+TDS:Upgrade(21)
+TDS:Upgrade(21)
+TDS:SetOption(21, "Flight Path", "Figure8")
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:SetOption(19, "Flight Path", "Figure8")
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:SetOption(16, "Flight Path", "Figure8")
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:SetOption(18, "Flight Path", "Figure8")
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+
+-- [ Wave 37 ] --
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:SetOption(17, "Flight Path", "Figure8")
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
+TDS:SetOption(20, "Flight Path", "Figure8")
+TDS:Upgrade(23)
+TDS:Upgrade(23)
+TDS:Upgrade(23)
+TDS:Upgrade(23)
+TDS:Upgrade(23)
+TDS:SetOption(23, "Flight Path", "Figure8")
+TDS:Upgrade(22)
+TDS:Upgrade(22)
+TDS:Upgrade(22)
+TDS:Upgrade(22)
+TDS:Upgrade(22)
+TDS:SetOption(22, "Flight Path", "Figure8")
+TDS:SetOption(4, "Track", "Red")
