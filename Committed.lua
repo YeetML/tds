6cf8df3325701b6f49be39093745e20dced3d751
YeetML <kongispro245@gmail.com>
2026-09-08 17:38:50 +0800

    Update Committed.lua

--- Committed.lua
+++ Committed.lua
@@ -1,15 +1,3 @@
-local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
-
-getgenv().AutoMedic = true
-getgenv().AutoMercenary = true
-getgenv().AutoDJ  = true
-getgenv().AutoGatling = true
-
-getgenv().AutoSkip = false
-getgenv().AutoReady = false
-getgenv().AutoRejoin = false
-getgenv().AutoRestart = false
-
 TDS:Loadout("Hacker", "Demoman", "Gatling Gun", "Scout", "Medic")
 TDS:Mode("Trial")
 
